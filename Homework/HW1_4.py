8f25c70accbf3c17e4030675a0d12e54d40f06c9
BadHobbit <BadHobbityyt@gmail.com>
2023-06-23 11:11:59 +0500

    HW_1 fixed

--- Homework/HW1_4.py
+++ Homework/HW1_4.py
@@ -1,6 +1,8 @@
-x = str(input())
-a = x[::-1]
-if x == a:
-    print('True')
-else:
-    print('False')
+def palindrom():
+    x = str(input())
+    a = x[::-1]
+    if x == a:
+        print('True')
+    else:
+        print('False')
+palindrom()
