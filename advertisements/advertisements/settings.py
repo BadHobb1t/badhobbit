b089c51658bef758f6fa00f5b4c9bf6e2383bbb0
BadHobbit <BadHobbityyt@gmail.com>
2023-08-24 21:17:24 +0500

    HW9-10_4

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_advertisements'
+    'app_advertisements',
+    'app_auth'
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     Path.joinpath(BASE_DIR, 'static')
 ]
+
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media/')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
-
+from .models import Advertisements
+from .forms import AdvertisementForm
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.decorators import login_required
 def index(request):
-    return render(request, 'index.html')
+    advertisements = Advertisements.objects.all()
+    context = {'advertisements': advertisements}
+    return render(request, 'app_advertisements/index.html', context)
 
 def top_sellers(request):
-    return render(request, 'top-sellers.html')
+    return render(request, 'app_advertisements/top-sellers.html')
+
+@login_required(login_url=reverse_lazy('login'))
+def advertisement_post(request):
+    if request.method == 'POST':
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisements = Advertisements(**form.cleaned_data)
+            advertisements.user = request.user
+            advertisements.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'app_advertisements/advertisement-post.html', context)
 
 # Create your views here.
